2b400242a4b3ae6bf12e79aa2bbcc1cee81c2da3
charrada <61417722+charrada@users.noreply.github.com>
2023-03-06 13:26:30 +0100

    new changes
    hello
    
    Co-Authored-By: Charrada1 <127068456+Charrada1@users.noreply.github.com>

--- console.js
+++ console.js
@@ -1,10 +1,14 @@
 console.log("Hello")
 console.log("World")
-function main(){
-return null;
+
+function main() {
+    return null;
 }
 main()
 console.log("World")
 console.log("World")
 console.log("World")
 console.log("World")
+
+const starting_stuff = ['1', '3', '5'];
+sorting_stuff.sort();
